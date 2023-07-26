80f8675233567b4ca7dfd8e9b13a234fda5cc288
BlackTanuTanuNinjas1035 <ickmkvo15@gmail.com>
2023-07-26 19:06:31 +0900

    メイン画面にカテゴリをグリッドで並べることにした

--- lib/algokit/categories.ex
+++ lib/algokit/categories.ex
@@ -5,10 +5,23 @@ defmodule Algokit.Categories do
   import Logger
 
   def init() do
-    if Enum.count(Repo.all(Category)) do
+    if Enum.count(Repo.all(Category)) == 0 do
+      Repo.insert %Category{name: "探索"}
+      Repo.insert %Category{name: "ソート"}
+      Repo.insert %Category{name: "グラフ"}
+      Repo.insert %Category{name: "パスファインディング"}
+      Repo.insert %Category{name: "衝突検出"}
+      Repo.insert %Category{name: "3Dレンダリング"}
+      Repo.insert %Category{name: "物理シミュレーション"}
+      Repo.insert %Category{name: "乱数生成"}
+      Repo.insert %Category{name: "なんとかアルゴリズム"}
       Repo.insert %Category{name: "なんとかアルゴリズム"}
     else
       Logger.info("すでに初期化済みです。")
     end
+  end
+
+  def list_categories() do
+    Repo.all Category
   end
 end
--- lib/algokit_web/live/main_menu_live.ex
+++ lib/algokit_web/live/main_menu_live.ex
@@ -1,18 +1,20 @@
 defmodule AlgokitWeb.MainMenuLive do
   use AlgokitWeb, :live_view
+  alias Algokit.Categories
 
   def render(assigns) do
     ~H"""
     <div class="h-full w-full">
       <p class="text-xl">メイン画面</p>
 
-      <div>
-        <select name="category" id="category">
-          <option value="1">カテゴリーID:1</option>
-          <option value="2">カテゴリーID:2</option>
-          <option value="3">カテゴリーID:3</option>
-        </select>
-      </div>
+      <div class="grid grid-cols-3 gap-4">
+          <%= for category <- @categories do %>
+            <!-- <button phx-click="select_category" phx-value-category_id={category.id} class="bg-blue-200 p-4"><%= category.name %></button> -->
+            <.link class="bg-blue-200 p-4" href={~p"/category/" <> "#{category.id}"}><%= category.name %></.link>
+          <% end %>
+        </div>
+
+
 
       <.link
         href={~p"/detail"}
@@ -30,6 +32,9 @@ defmodule AlgokitWeb.MainMenuLive do
   end
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    categories = Categories.list_categories()
+    {:ok, assign(socket,
+      categories: categories
+    )}
   end
 end
