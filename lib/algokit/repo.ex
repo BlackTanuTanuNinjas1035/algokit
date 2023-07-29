5d8bdcf7e58e67aadb3ec8f67e97a6ad492af114
BlackTanuTanuNinjas1035 <ickmkvo15@gmail.com>
2023-07-29 17:25:09 +0900

    /priv/static/migratinsを/lib/algokitに移動しファイル内のモジュール名を変更&exにする。repo.exのinitalize()にEcto.Migrator.upとデータベースの初期化するために作っている関数を実行させとけばOK

--- lib/algokit/repo.ex
+++ lib/algokit/repo.ex
@@ -3,8 +3,13 @@ defmodule Algokit.Repo do
     otp_app: :algokit,
     adapter: Ecto.Adapters.SQLite3
 
+  alias Algokit.Categories
+  alias Algokit.Algorithms
+
   def initialize() do
     Ecto.Migrator.up(Algokit.Repo, 20_230_726_062_245, Algokit.Migrations.CreateCategories)
     Ecto.Migrator.up(Algokit.Repo, 20_230_726_062_702, Algokit.Migrations.CreateAlgorithms)
+    Categories.init()
+    Algorithms.init()
   end
 end
