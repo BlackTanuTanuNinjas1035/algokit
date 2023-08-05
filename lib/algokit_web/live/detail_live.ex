821826f062ad4f90a16d3a27c3eb1903fa5f2c69
BlackTanuTanuNinjas1035 <ickmkvo15@gmail.com>
2023-08-05 16:15:01 +0900

    詳細ページを8割

--- lib/algokit_web/live/detail_live.ex
+++ lib/algokit_web/live/detail_live.ex
@@ -5,12 +5,13 @@ defmodule AlgokitWeb.DetailLive do
   require Logger
   def render(assigns) do
     ~H"""
-      <header class="flex justify-between py-2 px-2 max-[500px]:h-[10%]">
+      <header class="flex justify-between  py-2 px-2 max-[500px]:h-[10%] min-[500px]:h-[80%] align_bottom">
         <.link
           href={~p"/category/#{@category_id}"}
-          class="w-[10%] inline-block rounded-lg bg-yellow-400 px-4 py-2 shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+          style="background-image: url('/images/消しゴム.png'); background-size: cover; background-position: center;"
+          class="flex justify-center min-[500px]:text-2xl min-[500px]:pl-3 max-[500px]:w-[30%] min-[500px]:w-[20%] min-[500px]:max-h-[80px] flex items-center justify-center inline-block rounded-lg bg-yellow-400 shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
         >
-          戻る
+          <p class="bg-white  min-w-[40%] min-[500px]:p-3 text-center">戻る</p>
         </.link>
 
         <button class=" " phx-click="push_bookmark_button">
@@ -18,100 +19,117 @@ defmodule AlgokitWeb.DetailLive do
             {
               if @bookmark, do: ~p"/images/ICOOON_MONO/スターの枠アイコン(たぬ).png", else: ~p"/images/ICOOON_MONO/スターの枠アイコン.png"
             }
-            class="block w-full h-auto max-h-[50px] max-w-[50px]"
+            class="block w-full max-[500px]:h-auto min-[500px]:max-h-[100px] min-[500px]:max-w-[100px] max-[500px]:max-h-[50px] max-[500px]:max-w-[50px]"
           />
         </button>
       </header>
 
 
-      <div class="px-3 max-[500px]:h-[90%]  w-full">
+      <div class="max-[500px]:h-[90%] w-full  max-[500px]:px-2">
 
         <!-- 名前とカテゴリ -->
-        <div class="border-[3px] border-pink-200 my-2">
-          <div class="w-auto bg-white p-4 shadow-md">
-            <p class="text-3xl lg:text-4xl text-center text-black font-semibold mb-2"><%= @algorithm.name %></p>
-            <p class="text-base text-center text-gray-600"><%= "カテゴリー: #{@algorithm.category.name}" %></p>
+        <div class="flex justify-center mb-1">
+          <div class="max-w-max min-w-[40%] border-[3px] border-pink-200">
+            <div class="bg-white p-4 shadow min-[500px]">
+              <p class="text-3xl lg:text-4xl text-center text-black font-semibold mb-2"><%= @algorithm.name %></p>
+              <p class="text-base text-center text-gray-600"><%= "カテゴリー: #{@algorithm.category.name}" %></p>
+            </div>
           </div>
         </div>
 
         <!-- Description -->
-        <div class="">
+        <div class="w-full mb-2">
 
-          <div class="flex mr-2">
+          <div class="flex mr-2 min-[500px]:justify-center min-[500px]:my-1">
             <p class="text-3xl max-[500px]:text-2xl mr-2  border-b-2 border-gray-500">1. 説明</p>
             <p class="text-red-500 hidden max-[500px]:block text-sm pt-2">※クリックで詳細</p>
           </div>
 
           <!-- スマホ画面 -->
-          <div class="min-[500px]:hidden w-auto max-w-md mx-auto p-4 my-2 rounded-lg border-yellow-500 bg-white shadow-md">
+          <div class="min-[500px]:hidden w-auto mx-auto p-4 mt-2 rounded-lg border-yellow-500 bg-white shadow min-[500px]">
             <p onclick="toggleDescription()" class=" text-xl text-center cursor-pointer"
             >
               <%= String.slice(@algorithm.description, 0, 50) <> "..." %>
             </p>
           </div>
 
-          <!-- 詳細 -->
-          <div id="DescriptionContainer" class="description-container hidden z-20 fixed top-1/2 left-1/2 w-[100vw] h-[100vh] transform -translate-x-1/2 -translate-y-1/2 p-4 bg-gray-200 border-2 border-gray-300 rounded">
-            <div class="flex justify-between mb-2 h-[7%]">
-              <div class="text-2xl">説明</div>
-              <button onclick="toggleDescription()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                  戻る
-              </button>
-            </div>
-
-              <div class="h-[90%] overflow-auto bg-white">
-                <%= for line <- String.split(@algorithm.description, "\n") do %>
-                  <div class="text-base"><%= line %></div>
-                <% end %>
-              </div>
-          </div>
-
           <!-- 横画面 -->
-          <div class="h-[90%] overflow-auto bg-white max-[500px]:hidden">
-            <%= for line <- String.split(@algorithm.description, "\n") do %>
-              <div class="text-base"><%= line %></div>
-            <% end %>
-          </div>
-        </div>
-
-        <!-- Pseudocode -->
-        <div class="w-full">
-          <!-- <div class="text-3xl max-[500px]:text-xl">疑似コード</div> -->
-
-          <div class="flex justify-center w-full mb-2">
-            <button onclick="togglePseudocode()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              疑似コードを見る
-            </button>
-          </div>
-
-          <!-- 詳細 -->
-          <div id="PseudocodeContainer" class="pseudocode-container hidden fixed top-1/2 left-1/2 w-[100vw] h-[100%] transform -translate-x-1/2 -translate-y-1/2 p-4 bg-gray-200 border-2 border-gray-300 rounded">
-            <div class="flex justify-between mb-2 h-[7%]">
-              <div class="text-2xl text-center">疑似コード</div>
-              <button onclick="togglePseudocode()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 px-4 rounded">
-                  戻る
-              </button>
-            </div>
-
-            <div class="h-[90%] overflow-auto bg-white">
-              <%= for line <- String.split(@algorithm.pseudocode, "\n") do %>
-                <div class="text-base"><%= line %></div>
+          <div class="flex justify-center w-full max-[500px]:hidden ">
+            <div class="h-[90%] overflow-auto bg-white max-[500px]:hidden min-[500px]:w-[80%]  border-[3px] border-yellow-200">
+              <%= for line <- String.split(@algorithm.description, "\n") do %>
+                <div class="text-base min-[500px]:text-2xl"><%= line %></div>
               <% end %>
             </div>
           </div>
         </div>
 
-        <!-- Example -->
+        <!-- Discription 詳細 -->
+        <div id="DescriptionContainer" class="description-container hidden z-20 fixed top-1/2 left-1/2 w-[100vw] h-[100vh] transform -translate-x-1/2 -translate-y-1/2 p-4 bg-gray-200 border-2 border-gray-300 rounded">
+          <div class="flex justify-between mb-2 h-[7%]">
+            <div class="text-2xl">説明</div>
+            <button onclick="toggleDescription()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                戻る
+            </button>
+          </div>
 
-          <div class="max-w-md mx-auto p-4 rounded-lg border-yellow-500 bg-white shadow-md">
-            <!-- <div class="text-3xl font-semibold mb-1">使用例</div> -->
+            <div class="h-[90%] overflow-auto bg-white">
+              <%= for line <- String.split(@algorithm.description, "\n") do %>
+                <div class="text-base"><%= line %></div>
+              <% end %>
+            </div>
+        </div>
+
+        <!-- Pseudocode -->
+        <div class="w-full mb-2">
+
+          <div class="flex mr-2 min-[500px]:justify-center min-[500px]:my-1">
+            <p class="text-3xl max-[500px]:hidden max-[500px]:text-2xl mr-2  border-b-2 border-gray-500">2. 疑似コード</p>
+          </div>
+
+          <div class="flex justify-center w-full mb-2 min-[500px]:hidden">
+            <button onclick="togglePseudocode()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              疑似コードを見る
+            </button>
+          </div>
+
+          <!-- 横画面 -->
+          <div class="flex justify-center w-full max-[500px]:hidden">
+            <div class="h-[90%] overflow-auto bg-white max-[500px]:hidden min-[500px]:w-[80%] border-[3px] border-blue-200">
+              <%= for line <- String.split(@algorithm.pseudocode, "\n") do %>
+                <div class="text-base min-[500px]:text-2xl"><%= line %></div>
+              <% end %>
+            </div>
+          </div>
+
+        </div>
+        <!-- 詳細 -->
+          <div id="PseudocodeContainer" class="z-20 pseudocode-container hidden fixed top-1/2 left-1/2 w-[100vw] h-[100%] transform -translate-x-1/2 -translate-y-1/2 p-4 bg-gray-200 border-2 border-gray-300 rounded">
+          <div class="flex justify-between mb-2 h-[7%]">
+            <div class="text-2xl text-center">疑似コード</div>
+            <button onclick="togglePseudocode()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 px-4 rounded">
+                戻る
+            </button>
+          </div>
+
+          <div class="h-[90%] overflow-auto bg-white">
+            <%= for line <- String.split(@algorithm.pseudocode, "\n") do %>
+              <div class="text-base"><%= line %></div>
+            <% end %>
+          </div>
+        </div>
+
+        <!-- Example -->
+        <div class="overflow-y-scroll">
+          <div class="max-w min-[500px] mx-auto p-4 rounded-lg border-yellow-500 bg-white shadow min-[500px]:w-[80%]">
+            <div class="max-[500px]:text-base min-[500px]:text-xl font-semibold mb-1">使用例</div>
             <div class="aspect-w-16 aspect-h-9 rounded-lg overflow-hidden">
               <video controls autoplay loop class="w-full h-full">
                 <source src="/videos/calc_angle.mp4" type="video/mp4">
-                <!-- 他の動画フォーマットに対するsourceタグを追加することもできます -->
               </video>
             </div>
           </div>
+        </div>
+
       </div>
 
       <script>
